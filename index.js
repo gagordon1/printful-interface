294633ee42284216684d3a2429e61c05a9fce574
Garrett Gordon <gagordon@mit.edu>
2022-06-02 18:24:30 -0400

    successfully making requests to printful

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const axios = require('axios');
+
+
 
 dotenv.config();
 
@@ -7,9 +10,32 @@ const app = express();
 const port = process.env.PORT;
 
 app.get('/', (req, res) => {
-  res.send('Express + TypeScript Server');
+  res.send('Printful Interface');
+});
+
+//get my store's products from printful api
+app.get('/products', (req, res) =>{
+
+  let options = {
+    headers: {
+      Authorization: 'Bearer ' + process.env.PRINTFUL_AUTH_KEY
+    }
+  }
+
+  axios.get(process.env.PRINTFUL_PRODUCTS_ENDPOINT,options)
+    .then(response => {
+      console.log(`statusCode: ${response.status}`);
+      console.log(response.data["result"]);
+      res.send(response.data["result"]);
+    })
+    .catch(error => {
+      console.error(error);
+      res.send(error);
+    });
+
+
 });
 
 app.listen(port, () => {
-  console.log(`[server]: Server is running at https://localhost:${port}`);
+  console.log(`[server]: Server is running at http://localhost:${port}`);
 });
