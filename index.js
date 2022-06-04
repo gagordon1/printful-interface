603bc33565cbccd49c31257f129a7f525e040649
Garrett Gordon <gagordon@mit.edu>
2022-06-03 22:04:50 -0400

    updated backend

--- index.js
+++ index.js
@@ -15,47 +15,86 @@ const options = {
   }
 }
 
-const getProductDetails = (id) => {
-  axios.get(process.env.PRINTFUL_PRODUCTS_ENDPOINT + "/" + id, options)
-    .then(response => {
-      const product = response.data.result;
-      let productInfo = {
-                  "name" : product.sync_product.name,
-                  "image" : product.sync_product.thumbnail_url,
-                  "retailPrice" : product.sync_variants[0].retail_price
-                }
-      console.log(productInfo);
-      return productInfo;
-    })
-    .catch(error =>{
-      console.error(error);
-    })
+const getProductById = (itemId) => axios.get(
+  process.env.PRINTFUL_PRODUCTS_ENDPOINT + "/" +itemId,
+  options
+);
+
+const parseProductData = (data) =>{
+  return {
+    thumbnail : data.sync_product.thumbnail_url,
+    name : data.sync_product.name,
+    retailPrice : data.sync_variants[0].retail_price,
+    availableSizes : data.sync_variants.map(
+      variant => variant.sku.split('_').pop()
+    )
+  }
 }
+
 
 app.get('/', (req, res) => {
   res.send('Printful Interface');
 });
 
-//get my store's products from printful api
+// STORE PRODUCTS INTERFACE
+//[
+//    {
+//        id : Number,
+//        retailPrice : String,
+//        name : String,
+//        thumbnail : String
+//    }
+// ]
 app.get('/products', (req, res) =>{
-
-
-
   axios.get(process.env.PRINTFUL_PRODUCTS_ENDPOINT,options)
     .then(response => {
       return response.data["result"];
     })
     .then(products => {
-      const data = products.map( item => getProductDetails(item.id));
-      res.send(JSON.stringify(data));
-    })
+      const data = products.map( (item) => getProductById(item.id));
+      axios.all(data)
+        .then(axios.spread((...response) =>{
+          let data = response.map(
+            (item) => parseProductData(item.data.result)
+          );
+          console.log(data);
+          res.send(data);
+        }))
+        .catch(error => {
+          console.log(error);
+          res.send(error);
+        })
+      })
     .catch(error => {
-      console.error(error);
       res.send(error);
     });
+});
 
+//PRODUCT INTERFACE
+//{
+  // thumbnail : String
+  // name : String
+  // availableSizes : [String]
+// }
+app.get('/product-details/:id', (req, res) => {
+
+  axios.get(
+    process.env.PRINTFUL_PRODUCTS_ENDPOINT + "/" + req.params.id,
+    options
+    )
+  .then(
+    response => {
+      let result = response.data.result;
+      res.send(JSON.stringify(parseProductData(result)));
+    }
+  )
+  .catch(
+    error => res.send(error)
+    );
 
 });
+
+
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
