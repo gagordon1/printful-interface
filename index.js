65fc96b67ef03083ac12b74e6050fafe13a99536
Garrett Gordon <gagordon@mit.edu>
2022-06-14 16:12:56 -0400

    changed acceptable host

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ dotenv.config();
 const app = express();
 app.use(express.json())
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://webstore-frontend.herokuapp.com');
+  res.setHeader('Access-Control-Allow-Origin', 'https://www.side-chain.xyz/');
   res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
   res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
   next();
